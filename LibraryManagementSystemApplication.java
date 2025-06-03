a47655fefe24c945024ead71b61be661054af8e3
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:55:17 +0530

    Update LibraryManagementSystemApplication.java

--- LibraryManagementSystemApplication.java
+++ LibraryManagementSystemApplication.java
@@ -1,3 +1,8 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
     public static void main(String[] args) {
