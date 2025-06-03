2ac580bd57f5cb1a8a538e3ee718d675da2635ca
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:57:17 +0530

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,16 +1,23 @@
+package com.example.library.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
 @Entity
+@Data
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
 
-    private Long bookId;
-    private Long userId;
+    @ManyToOne
+    private Book book;
+
+    @ManyToOne
+    private User user;
 
     private LocalDate issueDate;
     private LocalDate returnDate;
-
-    private String status; // "Issued", "Returned", "Overdue"
-
-    // Getters & Setters
+    private String status;
 }
