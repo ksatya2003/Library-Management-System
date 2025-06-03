6c49c2a9f265bd86c08d8146e5e08fae3db69e29
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:42:57 +0530

    Create LibraryManagementSystemApplication.java

---
+++ LibraryManagementSystemApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemApplication.class, args);
+    }
+}
