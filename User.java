0e00782fdef1b1f181d070f52af7cbce686b12d6
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:56:58 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,10 +1,14 @@
+package com.example.library.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String name;
     private String membershipType;
-
-    // Getters & Setters
 }
