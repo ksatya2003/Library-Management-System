c381ab1a5bc9bd2fdd14de8bc130e3780eb6dd22
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:57:55 +0530

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,1 +1,6 @@
+package com.example.library.repository;
+
+import com.example.library.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface UserRepository extends JpaRepository<User, Long> {}
