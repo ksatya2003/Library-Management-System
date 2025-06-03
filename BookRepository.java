4bd03d33c4c2692ff66fec278d8bd165e8c82bd0
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:45:08 +0530

    Create BookRepository.java

---
+++ BookRepository.java
@@ -0,0 +1,5 @@
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+    List<Book> findByCategoryContainingIgnoreCase(String category);
+}
